131133541fe30b75a4f2a154d8683a89439c9c76
olaf0257 <snowman202057@gmail.com>
2022-02-11 14:27:46 +0200

    fix integration error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -432,7 +432,7 @@ protected
       end
       page[:count].replace_html :partial => 'shared/count' if display_count
       page[:spaces].replace_html :partial => 'space_pulldown'
-      page[:dates].replace_html :partial => 'shared/dates' if display_dates
+      # page[:dates].replace_html :partial => 'shared/dates' if display_dates
     end
   end
 

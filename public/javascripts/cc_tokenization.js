4f699f8c3f88508829f292ec457f20b27258042d
olaf0257 <snowman202057@gmail.com>
2022-03-10 04:43:10 +0200

    fix-paypal integration

--- public/javascripts/cc_tokenization.js
+++ public/javascripts/cc_tokenization.js
@@ -1,6 +1,6 @@
 // function for card connect tokenization message
-window.addEventListener('message', function(event) {
-  document.getElementById('mytoken').value = JSON.parse(event.data);
-  var token = JSON.parse(event.data);      
-  var mytoken = document.getElementById('mytoken');
-  mytoken.value = token.message; }, false);
+// window.addEventListener('message', function(event) {
+//   document.getElementById('mytoken').value = JSON.parse(event.data);
+//   var token = JSON.parse(event.data);      
+//   var mytoken = document.getElementById('mytoken');
+//   mytoken.value = token.message; }, false);

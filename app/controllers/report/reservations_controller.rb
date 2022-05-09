80604c78129ad6953831f149b2bbb753e62cfbb8
olaf0257 <snowman202057@gmail.com>
2022-05-09 13:11:43 +0300

    add remote reservations report

--- app/controllers/report/reservations_controller.rb
+++ app/controllers/report/reservations_controller.rb
@@ -20,12 +20,12 @@ class Report::ReservationsController < ApplicationController
       # now for the data
       result = get_reservations( startdate, enddate)
       result.each do |r|
-	csv_string << "\"#{r.space.name}\",\"#{r.startdate}\",\"#{r.enddate}\",\"#{r.camper.full_name}\",\"#{r.camper.address}\",\"#{r.camper.city}\",\"#{r.camper.state}\",\"#{r.camper.mail_code}\""
-	csv_string << ",\"#{r.total}\",\"#{r.tax_amount}\",\"#{Payment.total(r.id)}\",\"#{r.last_log_entry}\"\n"
+        csv_string << "\"#{r.space.name}\",\"#{r.startdate}\",\"#{r.enddate}\",\"#{r.camper.full_name}\",\"#{r.camper.address}\",\"#{r.camper.city}\",\"#{r.camper.state}\",\"#{r.camper.mail_code}\""
+        csv_string << ",\"#{r.total}\",\"#{r.tax_amount}\",\"#{Payment.total(r.id)}\",\"#{r.last_log_entry}\"\n"
       end
       send_data(csv_string, 
-		:type => 'text/csv;charset=iso-8859-1;header=present',
-		:disposition => 'attachment; filename=Occupancy.csv')
+        :type => 'text/csv;charset=iso-8859-1;header=present',
+        :disposition => 'attachment; filename=Occupancy.csv')
     else
       res = Reservation.new(params[:reservation])
       @startdate = res.startdate
@@ -36,9 +36,64 @@ class Report::ReservationsController < ApplicationController
       @result = get_reservations( @startdate, @enddate)
     end
   end
+
+  def res_date
+    @page_title = "Remote Reservations Report"
+  end
+
+  def remoteResReport
+    strtemp1 = params[:reservation]["startdate(1i)"].to_s + "-" + params[:reservation]["startdate(2i)"].to_s + "-" + params[:reservation]["startdate(3i)"].to_s
+    strtemp2 = params[:reservation]["enddate(1i)"].to_s + "-" + params[:reservation]["enddate(2i)"].to_s + "-" + params[:reservation]["enddate(3i)"].to_s
+    @startdate = Date.parse(strtemp1)
+    @enddate = Date.parse(strtemp2)
+
+    @result = get_remotereservations("false", @startdate, @enddate)
+
+    render :json => @result
+  end
+
+  def allResReport
+    strtemp1 = params[:reservation]["startdate(1i)"].to_s + "-" + params[:reservation]["startdate(2i)"].to_s + "-" + params[:reservation]["startdate(3i)"].to_s
+    strtemp2 = params[:reservation]["enddate(1i)"].to_s + "-" + params[:reservation]["enddate(2i)"].to_s + "-" + params[:reservation]["enddate(3i)"].to_s
+    @startdate = Date.parse(strtemp1)
+    @enddate = Date.parse(strtemp2)
+
+    @result = get_remotereservations(params[:status], @startdate, @enddate)
+
+    render :json => @result
+  end
+
 private
   def get_reservations(sd, ed)
     res = Reservation.all(:conditions => ["confirm = ? and startdate <= ? and enddate >= ?", true, ed, sd], 
                           :include => ["camper","space"], :order => :startdate)
+  end
+
+  def get_remotereservations(status, sd, ed)
+    if status == "true"
+      res = Reservation.all(:conditions => ["is_remote = ? and startdate <= ? and enddate >= ?", true, ed, sd],
+      :include => ["camper","space"], :order => :startdate)
+    else
+      res = Reservation.all(:conditions => ["is_remote = ? and unconfirmed_remote = ? and startdate <= ? and enddate >= ?", true, true, ed, sd],
+      :include => ["camper","space"], :order => :startdate)
+    end
+    
+    result = Array.new
+    for row in res do
+      if row.camper
+        full_name = row.camper.full_name
+        city = row.camper.city
+        state = row.camper.state
+        mail_code = row.camper.mail_code
+      else
+        full_name = ''
+        city = ''
+        state = ''
+        mail_code = ''
+      end 
+      temp = [row.space.name, row.startdate, row.enddate, full_name, city, state, mail_code, number_2_currency(row.total), number_2_currency(row.tax_amount), number_2_currency(Payment.total(row.id)), row.last_log_entry]
+      result << temp
+    end
+    return result
   end
 end

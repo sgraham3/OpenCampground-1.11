374016c72b90bb1dab4fcf6ec97c498e10f3f1f6
olaf0257 <snowman202057@gmail.com>
2022-07-19 04:05:22 +0300

    add-drop down selection for arrival time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -179,6 +179,19 @@ module ApplicationHelper
     end
   end
 
+  def arrival_time(param)
+    arrival_time = ["", "0:00am", "0:30am", "1:00am", "1:30am", "2:00am", "2:30am", "3:00am", "3:30am", "4:00am", "4:30am", "5:00am", "5:30am", "6:00am", "6:30am", "7:00am", "7:30am", "8:00am", "8:30am", "9:00am", "9:30am", "10:00am", "10:30am", "11:00am", "11:30am", "12:00pm", "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm", "4:30pm", "5:00pm", "5:30pm", "6:00pm", "6:30pm", "7:00pm", "7:30pm", "8:00pm", "8:30pm", "9:00pm", "9:30pm", "10:00pm", "10:30pm", "11:00pm", "11:30pm"]
+    str = "<select id='arrival_time'>"
+    arrival_time.map{ |time|
+      if param === time
+        str << "<option selected>" + time + "</option>"
+      else
+        str << "<option>" + time + "</option>"
+      end
+    }
+    str << "</select>"
+  end
+
   def charge_extra(e, use_discount, season_count)
     str = ''
     # str << "<td>extra_charge id #{e.id}</td><td>extra id #{e.extra_id}</td><td>#{e.extra.extra_type}</td><td>#{e.extra.charge}</td><td>#{e.charge}</td></tr><tr>"
